ca08f3e62a79541404713dd11506acb74e82b1f8
unrealwork <unrealwoork@gmail.com>
2017-12-15 00:59:57 +0300

    footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,38 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { SignupComponent } from './auth/signup/signup.component';
-import { HomeComponent } from './home/home.component';
+import {AppComponent} from './app.component';
+import {SignupComponent} from './auth/signup/signup.component';
+import {HomeComponent} from './home/home.component';
 import {RouterModule, Routes} from '@angular/router';
-import { HeaderComponent } from './header/header.component';
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
 
 const appRoutes: Routes = [
-  {
-    path: '',
-    component: HomeComponent
-  },
-  {
-    path: 'signup',
-    component: SignupComponent
-  }
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'signup',
+        component: SignupComponent
+    }
 ];
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SignupComponent,
-    HomeComponent,
-    HeaderComponent
-  ],
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot(appRoutes)
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        SignupComponent,
+        HomeComponent,
+        HeaderComponent,
+        FooterComponent
+    ],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot(appRoutes)
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
