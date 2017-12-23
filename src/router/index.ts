b6c0d97eccdcdfde0c1778df2feb96742265579c
unrealwork <unrealwork@gmail.com>
2017-12-23 09:15:59 +0300

    home

--- src/router/index.ts
+++ src/router/index.ts
@@ -12,7 +12,7 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld,
-      meta: {title: 'a'}
+      meta: {title: 'Home'}
     },
     {
       path: '/about',
